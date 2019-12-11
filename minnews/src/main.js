7cd969aeb4ee53e9e9c6f858d6c38f137a4211df
gaodl <gaodl@centaline.com.cn>
2019-12-11 16:07:20 +0800

    引入Vant

--- minnews/src/main.js
+++ minnews/src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant'
+import 'vant/lib/index.css';
 
 Vue.config.productionTip = false
+
+//使用插件
+Vue.use(Vant);
 
 new Vue({
   router,
